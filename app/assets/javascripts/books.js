f1c619582478ddb3fa3598d299dd71a941f55767
Sara Stanton <sara.mpa13@gmail.com>
2018-11-19 03:03:32 -0600

    small layout changes to Book show page for Add A Review form

--- app/assets/javascripts/books.js
+++ app/assets/javascripts/books.js
@@ -5,18 +5,13 @@ $(document).ready(function() {
       console.log(response);
       if(response.length != 0) {
         response.forEach(function(review) {
-          console.log(review["content"])
           $("#show-reviews").append(`<li> <strong> ${review["user"]["username"]} wrote: </strong> "${review["content"]}" </li> <br />`)
-          $("#show-reviews").append("<form class='button_to' method='get' action='/books/11/reviews/21/edit'><input type='submit' value='Edit Review'></form>")
-
-            // if(review.user == current_user) { //how to get current_user into JS
-            //   $("#show-reviews").append("<br /><form class='button_to' method='get' action='books/3/reviews/new'><input type='submit' value='Add Review'></form> <br />")
-            //  } else {
-            //    $("#show-reviews").append(`<%= @book.title %> does not have any reviews yet. Be the first to review this book!`)
-            //  }
-      console.log(response)
-        })
-      }
+          // how to check current user for edit button? UL in view page
+          // $("#show-reviews").append("<form class='button_to' method='get' action='/books/11/reviews/21/edit'><input type='submit' value='Edit Review'></form>")
+        });
+      } else {
+          $("#show-reviews").append(`<%= @book.title %> does not have any reviews yet. Be the first to review this book!`)
+        }
     })
   })
 })
