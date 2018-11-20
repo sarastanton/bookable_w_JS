a94449182ff53b1dc9377fdbb38599ee80e38ca2
Sara Stanton <sara.mpa13@gmail.com>
2018-11-20 02:16:26 -0600

    set up JS constructors and adapter for Authors index

--- app/assets/javascripts/authors.js
+++ app/assets/javascripts/authors.js
@@ -2,10 +2,46 @@
 //   <th> Author </th>
 //   <th> Number of Books </th>
 //
-//   <% @authors.each do |author| %>
-//   <tr>
-//     <td> <%= link_to author.name, author_path(author) %> </td>
-//     <td> <%= author.books.count %> </td>
+//   <tr div id="authors_index_container">
+//     <td> </td>
+//     <td> </td>
 //   </tr>
-//   <% end %>
 // </table>
+//////////////////////////////////////
+
+class AuthorsApp {
+  constructor() {
+    this.authors = new Authors()
+  }
+}
+
+const authorsApp = new AuthorsApp()
+
+class AuthorsAdapter {
+  constructor() {
+    this.baseUrl = 'http://localhost:3000/authors'
+  }
+
+  getAuthors() {
+    return fetch(this.baseUrl).then( response => response.json() )
+  }
+
+}
+
+class Authors {
+  constructor() {
+    this.authors = [] // Don't think I need this - array comes from controller?
+    this.adapter = new AuthorsAdapter()
+    // this.bindEventListeners()
+    this.fetchAndLoadAuthors()
+  }
+
+  fetchAndLoadAuthors() {
+    this.adapter.getAuthors().then(authors => {
+      console.log(authors)
+    })
+  }
+
+
+
+}
