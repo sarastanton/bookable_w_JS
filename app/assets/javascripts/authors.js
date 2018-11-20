e85fbbff5d11ef005f07e11cc0fcebce9863e2c9
Sara Stanton <sara.mpa13@gmail.com>
2018-11-20 02:48:12 -0600

    set up JS constructors and adapter for Authors index

--- app/assets/javascripts/authors.js
+++ app/assets/javascripts/authors.js
@@ -15,33 +15,42 @@ class AuthorsApp {
   }
 }
 
-const authorsApp = new AuthorsApp()
-
 class AuthorsAdapter {
+  // connects to API/backend
   constructor() {
-    this.baseUrl = 'http://localhost:3000/authors'
+    this.baseUrl = 'http://localhost:3000/authors.json'
   }
 
   getAuthors() {
-    return fetch(this.baseUrl).then( response => response.json() )
+    return fetch(this.baseUrl).then(response => response.json())
   }
 
 }
 
 class Authors {
   constructor() {
-    this.authors = [] // Don't think I need this - array comes from controller?
+    // this.authors = [] // Don't think I need this - array comes from controller?
     this.adapter = new AuthorsAdapter()
     // this.bindEventListeners()
     this.fetchAndLoadAuthors()
   }
 
   fetchAndLoadAuthors() {
-    this.adapter.getAuthors().then(authors => {
-      console.log(authors)
+    this.adapter
+    .getAuthors()
+    .then(authors => {
+      authors.forEach(author => renderAuthors(author))
     })
   }
 
+  renderAuthors(author) {
+    const $authorsColumn = $("td.col-1")
+    const $authorBookCount = $("td.col-2")
+    $authorsColumn.text(author["name"])
+    $authorBookCount.text(author["books"].length)
+  }
 
 
 }
+
+const authorsApp = new AuthorsApp()
