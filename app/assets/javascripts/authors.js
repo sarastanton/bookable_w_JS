202a7e858283252fcd022f3ed953233bdf3b141d
Sara Stanton <sara.mpa13@gmail.com>
2018-11-21 01:39:35 -0600

    changed fetch responses to Author model objects

--- app/assets/javascripts/authors.js
+++ app/assets/javascripts/authors.js
@@ -1,65 +1,71 @@
-class AuthorsApp {
-  constructor() {
-    this.authors = new Authors()
-  }
-}
+$(document).ready(function() {
 
-class AuthorsAdapter {
-  // connects to API/backend
-  constructor() {
-    this.baseUrl = 'http://localhost:3000/authors.json'
-  }
-
-  getAuthors() {
-    return fetch(this.baseUrl).then(response => response.json())
-  }
-
-  createDBAuthor(name) {
-    const author = {
-      body: name,
+  class AuthorsApp {
+    constructor() {
+      this.authors = new Authors()
     }
-    return fetch(this.baseUrl, {
-      method: 'POST',
-      body: JSON.stringify({ author }),
-      headers: { "content-type": "application/json" }
-    })
   }
 
-}
+  class AuthorsAdapter {
+    // connects to API/backend
+    constructor() {
+      this.baseUrl = 'http://localhost:3000/authors.json'
+    }
 
-class Authors {
-  constructor() {
-    this.adapter = new AuthorsAdapter()
-    // this.bindEventListeners()
-    this.fetchAndLoadAuthors()
-    this.createNewAuthor()
+    getAuthors() {
+      return fetch(this.baseUrl).then(response => response.json())
+    }
+
+    createDBAuthor(name) {
+      const author = {
+        body: name,
+      }
+      return fetch(this.baseUrl, {
+        method: 'POST',
+        body: JSON.stringify({ author }),
+        headers: { "content-type": "application/json" }
+      })
+    }
+
   }
 
-    // bindEventListeners() {
-    //
+  class Authors {
+    constructor() {
+      this.adapter = new AuthorsAdapter()
+      // this.bindEventListeners()
+      this.fetchAndLoadAuthors()
+      this.createNewAuthor()
+      this.authors = []
+    }
+
+      // bindEventListeners() {
+      //
+      // }
+
+    fetchAndLoadAuthors() {
+      this.adapter
+      .getAuthors()
+      .then(authors => {
+        authors.forEach(author => this.authors.push(new Author(author)))
+      })
+      .then(authors => {
+        this.renderAuthors()
+      })
+    }
+
+    renderAuthors() {
+      debugger
+      this.authors.forEach(author =>
+      $("#authors_index_container").append(`<tr><td>${author["name"]}</td><td>${author["books"].length}</td></tr>`)
+    )}
+
+    // createNewAuthor(e) {
+    //   e.preventDefault();
+    //   const form_input = this.formInput;
+    //   this.adapter.createDBAuthor(form_input).then(author => console.log(author))
+    //   // this.renderAuthors()
     // }
-
-  fetchAndLoadAuthors() {
-    this.adapter
-    .getAuthors()
-    .then(authors => {
-      this.renderAuthors(authors)
-    })
-  }
-
-  renderAuthors(authors) {
-    authors.forEach(author =>
-    $("#authors_index_container").append(`<tr><td>${author["name"]}</td><td>${author["books"].length}</td></tr>`)
-  )}
-
-  // createNewAuthor(e) {
-  //   e.preventDefault();
-  //   const form_input = this.formInput;
-  //   this.adapter.createDBAuthor(form_input).then(author => console.log(author))
-  //   // this.renderAuthors()
-  // }
-  createNewAuthor() {
-    $(document).ready(function() {
+    createNewAuthor() {
       $("#new_author").on("submit", function(e) {
         e.preventDefault();
         formInput = $("#author_name").val();
@@ -67,11 +73,19 @@ class Authors {
         // this.adapter.createDBAuthor(form_input)
         // fetchAndLoadAuthors()
       })
-    })
+    }
+
   }
 
+  class Author {
+    constructor(authorJSON) {
+      this.id = authorJSON.id
+      this.name = authorJSON.name
+      this.books = authorJSON.books
+      this.genres = authorJSON.genres
+    }
+  }
 
+  const authorsApp = new AuthorsApp()
 
-}
-
-const authorsApp = new AuthorsApp()
+})
