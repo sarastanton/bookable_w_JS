9127fce259124952d0a4a5c1f09b6da73a0d35f9
Sara Stanton <sara.mpa13@gmail.com>
2019-03-03 03:27:46 -0600

    changed id fields to integer for transition to postgres from sqlite

--- db/migrate/20180831034139_create_books.rb
+++ db/migrate/20180831034139_create_books.rb
@@ -2,10 +2,10 @@ class CreateBooks < ActiveRecord::Migration[5.2]
   def change
     create_table :books do |t|
       t.string :title
-      t.string :author_id
-      t.string :genre_id
+      t.integer :author_id
+      t.integer :genre_id
       t.integer :page_count
-      
+
       t.timestamps
     end
   end
