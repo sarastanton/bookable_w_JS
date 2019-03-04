41860dc1eedad95a524ab41b48f97f126ab50a29
Sara Stanton <sara.mpa13@gmail.com>
2019-03-04 02:25:34 -0600

    changed omniauth-goodreads2 gem to omniauth-goodreads3

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :goodreads2, ENV['GOODREADS_KEY'], ENV['GOODREADS_SECRET']
+  provider :goodreads3, ENV['GOODREADS_KEY'], ENV['GOODREADS_SECRET']
 end
